dee399b1c1725f0a0f8e1cf4b75082aea6ffd2c6
Ryo Nakamura <r7kamura@gmail.com>
2022-01-15 19:44:39 +0900

    Add test for PathFinder

---
+++ spec/templatecop/path_finder_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+RSpec.describe Templatecop::PathFinder do
+  describe '#call' do
+    subject do
+      described_class.new(
+        default_patterns: default_patterns,
+        patterns: patterns
+      ).call
+    end
+
+    let(:default_patterns) do
+      %w[**/*.slim]
+    end
+
+    let(:patterns) do
+      raise NotImplementedError
+    end
+
+    context 'with normal file path' do
+      let(:patterns) do
+        %w[spec/fixtures/dummy.slim]
+      end
+
+      it 'returns expected file paths' do
+        is_expected.to eq(patterns)
+      end
+    end
+
+    context 'with glob pattern' do
+      let(:patterns) do
+        %w[spec/**/*.slim]
+      end
+
+      it 'returns expected file paths' do
+        is_expected.to eq(%w[spec/fixtures/dummy.slim])
+      end
+    end
+
+    context 'with directory path' do
+      let(:patterns) do
+        %w[spec/fixtures]
+      end
+
+      it 'excludes directies' do
+        is_expected.to be_empty
+      end
+    end
+
+    context 'with empty patterns' do
+      let(:patterns) do
+        []
+      end
+
+      it 'uses default patterns' do
+        is_expected.to eq(%w[spec/fixtures/dummy.slim])
+      end
+    end
+  end
+end
